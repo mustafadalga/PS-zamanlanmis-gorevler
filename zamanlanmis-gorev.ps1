0cac28eb318ba5ce9f950203720cfd7b60f10745
mustafadalga <25087769+mustafadalga@users.noreply.github.com>
2019-01-14 10:40:37 +0300

    Add files via upload

---
+++ zamanlanmis-gorev.ps1
@@ -0,0 +1,4 @@
+﻿$uygulama    = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument 'rm -Recurse -Force $env:APPDATA\Microsoft\Windows\Recent\*'
+$saat  = New-ScheduledTaskTrigger -Daily -At 23:00pm
+$ayar = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME" -LogonType "S4U" -RunLevel Highest
+Register-ScheduledTask -TaskName "Gorev" -Action $uygulama -Trigger $saat -Principal $ayar -Force
